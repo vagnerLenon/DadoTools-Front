af3994dfdcf9c07826110dcf08902489130d1d4b
semctrlz <vagner.lenon@gmail.com>
2020-08-07 11:40:43 -0300

    Atualizado sistema de cadasto

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,7 @@ import persistReducers from './persistReducers';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 // eslint-disable-next-line operator-linebreak
+
 const sagaMonitor =
   process.env.NODE_ENV === 'development'
     ? console.tron.createSagaMonitor()
